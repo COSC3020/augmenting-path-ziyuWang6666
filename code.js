8840d0362b557d6338bf041e69de13c5f9ef53d1
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-11-09 21:02:20 -0700

    Update code.js
    need help

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
+var graph1 = {'foo': {'boo': 7},
+             'boo': {'foo': 3, 'bar': 2},
+             'bar': {'boo': 4}};
+
 function augmentingPath(graph, start, end) {
+    // Hi Lars, do you mean I need convert the graph to adjacency list first?
+    // for example, when take graph1 as a parameter, to convert to a adjacency list first?
     return [];
 }
+augmentingPath(graph1, 'foo', 'bar')
