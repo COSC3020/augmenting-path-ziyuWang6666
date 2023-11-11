bdb2a2690280ec2a62aca99b23870a2fca9e12e5
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-11-10 18:07:06 -0700

    Update code.js
    add augmenting path

--- code.js
+++ code.js
@@ -1,10 +1,56 @@
-var graph1 = {'foo': {'boo': 7},
-             'boo': {'foo': 3, 'bar': 2},
-             'bar': {'boo': 4}};
-
-function augmentingPath(graph, start, end) {
-    // Hi Lars, do you mean I need convert the graph to adjacency list first?
-    // for example, when take graph1 as a parameter, to convert to a adjacency list first?
-    return [];
+ 
+var graph1 =  {'foo': {'boo': 7, 'd': 4},
+'boo': {'e': 3},
+'d': {'bar': 4},
+'e': {'foo': 1},
+'bar': {'foo': 2}};
+class Graph {
+    constructor() {
+      this.adjList = [];
+    }
+  
+    addNode(node) {
+      if (!this.adjList[node]) {
+        this.adjList[node] = [];
+      }
+    }
+  
+    addEdge(startNode, endNode) {
+      this.adjList[startNode].push(endNode);
+    }
 }
-augmentingPath(graph1, 'foo', 'bar')
+function depthFirstSearch(graph, startNode, targetNode) {
+    // console.log(graph)
+    const stack = [startNode];
+    const visited = new Set();
+    const paths = {};
+    paths[startNode] = [startNode];
+  
+    while (stack.length > 0) {
+      const vertex = stack.pop();
+      visited.add(vertex);
+        for (const neighbor of graph.adjList[vertex]) {
+          if (!visited.has(neighbor)) {
+            stack.push(neighbor);
+            paths[neighbor] = paths[vertex].concat(neighbor);
+            if (neighbor === targetNode) {
+              return paths[neighbor];
+            }
+          }
+        }
+    }
+    return []
+}
+function augmentingPath(graph, start, end) {
+    // const nodes = new Set(Object.keys(graph));
+    const adjList = new Graph();
+    for(const node in graph) {
+        adjList.addNode(node);// add node to adjList
+        for(const siblings in graph[node]) {
+            adjList.addEdge(node, siblings);
+        }
+    }
+    return depthFirstSearch(adjList, start, end);
+}
+
+// console.log(augmentingPath(graph1, 'foo', 'foo'));
